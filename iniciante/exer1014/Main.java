60b1fc40cac1278a9b2a3dd6b1c30a01609d16ee
your username <daniel.fernandez@unigranrio.br>
2020-06-10 15:40:08 -0300

    Quinto commit

---
+++ iniciante/exer1014/Main.java
@@ -0,0 +1,18 @@
+package exer1014;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		double y = scan.nextDouble();
+		
+		double totalGasto = x / y;
+		
+		System.out.printf("%.3f km/l%n", totalGasto);
+	}
+
+}
---
+++ iniciante/exer1016/Main.java
@@ -0,0 +1,18 @@
+package exer1016;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int distancia = scan.nextInt();
+		
+		int tempo = distancia * 2;
+		
+		
+		System.out.println(tempo + " minutos");
+	}
+
+}
